3265cdd8144871e73c9d7a815601917cc2b52ef4
Bogdan Radenkov <bobkata88@gmail.com>
2022-09-27 13:04:39 +0300

    eddited to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const mongodb = request('mongodb');
-const connectionString = 'mongodb://localhost:27017'; 
-const connection = new mongodb.MongoClient(connectionString, {
-    useUnifiedTopology: true;
-});
-connection.connect();
+const mongoose = require('mongoose');
+const Person = require('./models/Person');
+const connectionString = 'mongodb://localhost:27017/testdb'; 
+start();
+async function start(){
+    await mongoose.connect(connectionString, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+    console.log('Database connected');
+
+    const data = await Person.find({});
+    console.log(data);
+}
---
+++ models/Person.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+const personSchema = new Schema({
+    name: String,
+    age: Number
+
+});
+const Person = model('Person', personSchema);
+
+module.exports = Person;
